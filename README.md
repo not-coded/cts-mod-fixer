b03bee0f71a8d55e5770e8ea5d36168bd879afc9
NotCoded <66999075+not-coded@users.noreply.github.com>
2025-08-15 19:12:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Combat Test 8c Mod Fixer
+
+Requires:
+- [Fabric API 0.42.0](https://github.com/not-coded/fabric/releases)
+
+Fixes:
+- Iris
---
+++ src/main/java/net/notcoded/modfixer/ModFixer.java
@@ -0,0 +1,10 @@
+package net.notcoded.modfixer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModFixer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
